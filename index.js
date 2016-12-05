3ffa09e0cb726854b7fac0540a0a1419a7a19aac
boeckMt <bbmatdd@gmail.com>
2016-12-06 00:11:32 +0100

    add body parser

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
+var bodyParser = require('body-parser');
 server.use(middlewares);
 server.get('/echo', function (req, res) {
     res.jsonp(req.query);
 });
+server.use(bodyParser.json({ limit: '10mb', extended: false }));
+server.use(bodyParser.urlencoded({ extended: false }));
 server.use(function (req, res, next) {
     if (req.method === 'POST') {
         req.body.createdAt = Date.now();
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ var jsonServer = require('json-server');
 var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
+var bodyParser = require('body-parser');
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -13,6 +14,8 @@ server.get('/echo', (req, res)=>{
   res.jsonp(req.query)
 });
 
+server.use(bodyParser.json({limit: '10mb', extended: false}));
+server.use(bodyParser.urlencoded({extended: false}));
 server.use((req, res, next)=>{
   if (req.method === 'POST') {
     req.body.createdAt = Date.now()
@@ -20,6 +23,23 @@ server.use((req, res, next)=>{
   // Continue to JSON Server router
   next()
 });
+
+
+
+//POST
+// header: Content-Type: application/json
+// body:
+/*
+{
+    "id": 6,
+    "country": "usa",
+    "properties": {
+      "name": "Muli",
+      "age": "34"
+    }
+  }
+*/
+
 
 // Use default router
 server.use(router)
